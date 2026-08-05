c24958a5d8469d64cc0701b36c5459ac0c8ea8ab
Vinod Bavage <bavagevinod604@gmail.com>
2026-08-05 20:37:47 +0530

    made some changes for render to work

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ app.add_middleware(
         "http://localhost:8080",
         "http://127.0.0.1:8080",
     ],
-    allow_origin_regex=r"https://.*\.onrender\.com",
+    allow_origin_regex=r"https://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
